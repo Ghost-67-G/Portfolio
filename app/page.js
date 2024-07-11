91b497b4f8f1f48c6365a4d912acd3ff8b7fd4cc
Ghost-67-G <ayan@ilmiya.com>
2024-07-11 18:03:54 +0500

    FIx

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
-import { personalData } from "@/utils/data/personal-data";
+// app/page.js
+import { personalData } from "../utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
@@ -8,24 +9,23 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-async function getBlogData() {
-  if (personalData.devUsername) {
-
-    const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
-
+async function getBlogData(devUsername) {
+  if (devUsername) {
+    const res = await fetch(`https://dev.to/api/articles?username=${devUsername}`, {
+      cache: 'no-store'
+    });
     if (!res.ok) {
-      throw new Error('Failed to fetch data')
+      throw new Error('Failed to fetch data');
     }
 
     const data = await res.json();
-
     const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
 
     return filtered;
   } else {
-    return
+    return [];
   }
-};
+}
 
 const getData = async () => {
   const url = 'https://api.jsonsilo.com/843eeaf1-64b1-49a9-be43-59c545d34bca';
@@ -33,14 +33,14 @@ const getData = async () => {
     'X-SILO-KEY': process.env.NEXT_PUBLIC_X_SILO_KEY,
     'Content-Type': 'application/json'
   };
-  const res = await fetch(url, { headers })
-  const data = await res.json()
-  return data
+  const res = await fetch(url, { headers, cache: 'no-store' });
+  const data = await res.json();
+  return data;
 }
 
 export default async function Home() {
-  const data = await getData()
-  const blogs = await getBlogData();
+  const data = await getData();
+  const blogs = await getBlogData(personalData.devUsername);
 
   return (
     <>
@@ -50,10 +50,8 @@ export default async function Home() {
       <Skills skillsData={data?.skills} />
       <Projects projectsData={data?.projects} />
       <Education educations={data?.educations} />
-      {blogs &&
-        <Blog blogs={blogs} />
-      }
+      {blogs && <Blog blogs={blogs} />}
       <ContactSection />
     </>
-  )
-};
+  );
+}
