1415aed55d2bf2a1d411ac5e9864a5383e044eac
Ghost-67-G <ayan@ilmiya.com>
2024-07-19 11:12:44 +0500

    Add seo things

--- app/layout.js
+++ app/layout.js
@@ -35,6 +35,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="aLIaDF4hw2F9DQ8adjH9okk3hc2uT1zFVBZmdBp-wlU" />
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Person",
+            "name": "Ayan Naseer",
+            "url": "https://portfolio-ayan.netlify.app/",
+            "sameAs": [
+              "https://github.com/ghost-67-g",
+              "https://www.linkedin.com/in/ayan-naseer/"
+            ],
+            "jobTitle": "MERN Stack Developer",
+            "description": "My name is Ayan Naseer. I am a dedicated and enthusiastic MERN Stack Developer with a passion for technology and problem-solving. I thrive on learning and exploring new technologies, driven by a self-motivated attitude and a quick learning ability. My expertise lies in JavaScript, enabling me to build robust and scalable web applications. I am committed to making the web more accessible and open to everyone. Whether it's frontend, backend, or full-stack development, I am eager to contribute to exciting projects that align with my skills and interests. I am available for job opportunities that offer growth and challenge in the field of web development.",
+            "image": 'https://i.postimg.cc/SxLQWxjv/studio-cbccf4021843dcf3fa76347421d6a889-wsighyog.jpg'
+          })}
+        </script>
+      </head>
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://portfolio-ayan.netlify.app',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  };
+  
